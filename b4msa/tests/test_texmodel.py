d96ab3c52d6a86de86a19fabc918ee669e74a1e7
Mario Graff <mgraffg@ieee.org>
2016-06-02 11:13:31 -0500

    examples - text.json

---
+++ b4msa/tests/test_texmodel.py
@@ -0,0 +1,20 @@
+# Copyright 2016 Mario Graff (https://github.com/mgraffg)
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def test_tweet_iterator():
+    import os
+    from b4msa.textmodel import tweet_iterator
+    dirname = os.path.dirname(__file__)
+    [x for x in tweet_iterator(dirname + '/text.json')]
--- b4msa/textmodel.py
+++ b4msa/textmodel.py
@@ -1,17 +1,30 @@
+# Copyright 2016 Eric S. Tellez
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import re
 import os
 import sys
-import shutil
 import json
 import unicodedata
 import gzip
 import random
 import logging
 import numpy as np
-from gensim import corpora, models
+from gensim import corpora
 from gensim.models.tfidfmodel import TfidfModel
 import pickle
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s : %(levelname)s :%(message)s',
+                    level=logging.INFO)
 
 
 def tweet_iterator(filename):
