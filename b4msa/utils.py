50fe2783ac0de97868e95029af21a2bc9b9f0d93
Mario Graff <mgraffg@ieee.org>
2016-06-08 14:01:48 -0500

    minor modifications in utils.py

--- b4msa/utils.py
+++ b4msa/utils.py
@@ -17,7 +17,7 @@ import gzip
 import logging
 
 import xml.etree.ElementTree as ET
-
+# from xml.dom import minidom
 logging.basicConfig(format='%(asctime)s : %(levelname)s :%(message)s',
                     level=logging.INFO)
 
@@ -27,43 +27,43 @@ def tweet_iterator(filename):
 
     # The file is a gz file (TODO: Test what is the content)...
     if filename.endswith(".gz"):
-	# Uncompress the file and open it...
+        # Uncompress the file and open it...
         f = gzip.GzipFile(filename)
         # Use JSON generator...
         return JSON_generator(f)
     # The file is an XML file...
     elif filename.endswith(".xml"):
-	# Open the file...
+        # Open the file...
         f = ET.parse(filename).getroot()
         # Use XML generator...
         return XML_generator(f)
     # JSON or unknown file format, guess...
     else:
-	# Not JSON extension, show warning...
+        # Not JSON extension, show warning...
         if not filename.endswith(".json"):
             # Print warning to user...
             print("WARNING! File extension not supported (" + filename.split(".")[-1] + ")")
             print("Assuming JSON format: {\"text\":, \"klass\":}")
         # Open the file...
         f = open(filename, encoding='utf8')
-	# Use JSON generator...
+        # Use JSON generator...
         return JSON_generator(f)
+
 
 # Get tweets from an XML formatted file (e.g. TASS 2015)...
 def XML_generator(f):
 
     # Start the iterator...
     for child in f:
-	# Get the content...
+        # Get the content...
         content = child.find('content').text
-	# Get the class...
+        # Get the class...
         klass = child.find('sentiments').find('polarity').find('value').text
-	# Create an array compatible with JSON
+        # Create an array compatible with JSON
         t = {'text': content, 'klass': klass}
-	# Return the element... 
+        # Return the element...
         yield t
 
-	
 
 # Get tweets from a JSON formated file (e.g. {"text":, "klass":})
 def JSON_generator(f):
@@ -87,26 +87,26 @@ def JSON_generator(f):
         t = None
 
         try:
-            # Try to get data from JSON format...    
+            # Try to get data from JSON format...
             t = get_tweet(line)
             # Return the generator...
             yield t
         # Catch the JSON Decode Error...
         except (json.decoder.JSONDecodeError, ValueError):
             # Print warning to user...
-            print("WARNING! we found and error while parsing file:", filename)
+            print("WARNING! we found and error while parsing file:", str(f))
             print("most of these errors occur due to concurrent writes")
 
     # Close the file...
     f.close()
 
 
-
 def get_tweet(line):
     return json.loads(line)
 
 
-def read_data_labels(filename, get_tweet='text', get_klass='klass', maxitems=1e100):
+def read_data_labels(filename, get_tweet='text',
+                     get_klass='klass', maxitems=1e100):
     data, labels = [], []
     count = 0
     for tweet in tweet_iterator(filename):
@@ -134,13 +134,11 @@ def read_data(filename, get_tweet='text', maxitems=1e100):
     return data
 
 
-from xml.dom import minidom
 def read_dataXML(filename, get_tweet='text', maxitems=1e100):
     data = []
     count = 0
-    XML_File = open(filename, 'r')
-    XML_Doc = minidom.parse(XML_File)
-
+    # XML_File = open(filename, 'r')
+    # XML_Doc = minidom.parse(XML_File)
 
     for tweet in tweet_iterator(filename):
         count += 1
@@ -150,4 +148,3 @@ def read_dataXML(filename, get_tweet='text', maxitems=1e100):
             break
 
     return data
-
