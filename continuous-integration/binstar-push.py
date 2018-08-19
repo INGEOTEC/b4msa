f02e7f1d8a20ca3d12887b33bca1b0836f2af890
Mario Graff <mgraffg@ieee.org>
2018-08-19 00:18:35 -0500

    python 3.7

--- continuous-integration/binstar-push.py
+++ continuous-integration/binstar-push.py
@@ -1,7 +1,8 @@
 import os
 import glob
-import subprocess
-import traceback
+# import subprocess
+# import traceback
+from binstar_client.scripts import cli
 
 
 def get_token():
@@ -13,9 +14,11 @@ def get_token():
 
 token = get_token()
 if token is not None:
-    cmd = ['binstar', '-t', token, 'upload', '--force', '-u', 'ingeotec']
+    cmd = ['-t', token, 'upload', '--force', '-u', 'ingeotec']
     cmd.extend(glob.glob('*.tar.bz2'))
-    try:
-        subprocess.check_call(cmd)
-    except subprocess.CalledProcessError:
-        traceback.print_exc()
+    cli.main(args=cmd)
+    # try:
+    #     print('*', cmd, platform.system())
+    #     subprocess.check_call(cmd)
+    # except subprocess.CalledProcessError:
+    #     traceback.print_exc()
