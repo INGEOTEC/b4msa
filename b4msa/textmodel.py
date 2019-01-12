beca318fdf77fb13e33b778bb5b12ba71f9d5a8a
Mario Graff <mgraffg@ieee.org>
2019-01-12 12:47:49 -0600

    Cleaning

--- b4msa/textmodel.py
+++ b4msa/textmodel.py
@@ -11,24 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import re
 import os
-import unicodedata
 from microtc.textmodel import TextModel as mTCTextModel
-from .params import OPTION_DELETE, OPTION_GROUP, OPTION_NONE, get_filename
+from microtc.params import OPTION_NONE, get_filename
 from .lang_dependency import LangDependency
-from .utils import tweet_iterator
-from collections import defaultdict
 import pickle
 import numpy as np
 import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s :%(message)s')
-
-PUNCTUACTION = ";:,.@\\-\"'/"
-SYMBOLS = "()[]¿?¡!{}~"
-SKIP_SYMBOLS = set(PUNCTUACTION + SYMBOLS)
-# SKIP_WORDS = set(["…", "..", "...", "...."])
 
 
 class TextModel(mTCTextModel):
@@ -36,30 +27,8 @@ class TextModel(mTCTextModel):
 
     :param docs: Corpus
     :type docs: lst
-    :param del_diac: Remove diacritics
-    :type del_diac: bool
-    :param num_option: Transformations on numbers
-    :type num_option: str
-    :param usr_option: Transformations on users
-    :type usr_option: str
-    :param url_option: Transformations on urls
-    :type url_option: str
-    :param emo_option: Transformations on emojis and emoticons
-    :type emo_option: str
-    :param lc: Lower case
-    :type lc: bool
-    :param del_dup: Remove duplicates e.g. hooola -> hola
-    :type del_dup: bool
-    :param token_list: Tokens > 0 qgrams < 0 word-grams
-    :type token_list: lst
-    :param lang: Language
-    :type lang: str
-    :param weighting: Weighting scheme
-    :type weighting: class or str
     :param threshold: Threshold to remove those tokens less than 1 - entropy
     :type threshold: float
-    :param token_min_filter: Keep those tokens that appear more times than the parameter (used in weighting class)
-    :type token_min_filter: int or float
     :param lang: Language (spanish | english | italian | german)
     :type lang: str
     :param negation: Negation
@@ -85,11 +54,8 @@ class TextModel(mTCTextModel):
      (77, 0.24737436144422534),
      (78, 0.24737436144422534)]
     """
-    def __init__(self, *args, del_diac=True,
-                 num_option=OPTION_GROUP, usr_option=OPTION_GROUP,
-                 url_option=OPTION_GROUP, emo_option=OPTION_GROUP,
-                 lc=True, del_dup=True, token_list=[-2, -1, 3, 4],
-                 lang=None, threshold=0, negation=False, stemming=False,
+    def __init__(self, docs, token_list=[-2, -1, 3, 4],
+                 threshold=0, lang=None, negation=False, stemming=False,
                  stopwords=OPTION_NONE, **kwargs):
         if lang:
             self.lang = LangDependency(lang)
@@ -98,9 +64,7 @@ class TextModel(mTCTextModel):
 
         self._threshold = threshold
         self._lang_kw = dict(negation=negation, stemming=stemming, stopwords=stopwords)
-        super(TextModel, self).__init__(*args, del_diac=del_diac, num_option=num_option,
-                                        usr_option=usr_option, url_option=url_option, emo_option=emo_option,
-                                        lc=lc, del_dup=del_dup, token_list=token_list, **kwargs)
+        super(TextModel, self).__init__(docs, token_list=token_list, **kwargs)
 
     def fit(self, X):
         """
@@ -142,22 +106,6 @@ class TextModel(mTCTextModel):
             logc = logc / np.log2(nklasses)
         return (1 + (weight * logc).sum(axis=0))
 
-    def __str__(self):
-        """String representation"""
-
-        return "[TextModel {0}]".format(dict(
-            strip_diac=self.strip_diac,
-            num_option=self.num_option,
-            usr_option=self.usr_option,
-            url_option=self.url_option,
-            emo_option=self.emo_option,
-            lc=self.lc,
-            del_dup=self.del_dup,
-            token_list=self.token_list,
-            lang=self.lang,
-            kwargs=self.kwargs
-        ))
-
     def extra_transformations(self, text):
         """Language dependent transformations
 
@@ -195,12 +143,4 @@ def get_model(basename, data, labels, args):
             pickle.dump(model, f)
     else:
         model = load_model(modelfile)
-
     return model
-
-
-# if __name__ == '__main__':
-#     filename = sys.argv[1]
-#     from .utils
-#     for kwargs in sample:
-#         get_model(filename, data, labels, kwargs)
