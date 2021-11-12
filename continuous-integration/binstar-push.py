abd981e43d7b2471ec3c6edd386b21239ca32544
Mario Graff <mgraffg@ieee.org>
2021-11-12 05:33:00 -0500

    Actions

--- continuous-integration/binstar-push.py
+++ continuous-integration/binstar-push.py
@@ -6,17 +6,13 @@ from binstar_client.scripts import cli
 
 
 def get_token():
-    token = None
-    if os.environ.get('TRAVIS_BRANCH', None) == 'master' or os.environ.get('APPVEYOR_REPO_BRANCH', None) == 'master':
-        token = os.environ.get('BINSTAR_TOKEN', None)
-    return token
+    return os.environ.get('ANACONDA', None)
 
 
 token = get_token()
 if token is not None:
     cmd = ['-t', token, 'upload', '--force', '-u', 'ingeotec']
     cmd.extend(glob.glob('*.tar.bz2'))
-    # print(cmd)
     cli.main(args=cmd)
     # try:
     #     print('*', cmd, platform.system())
